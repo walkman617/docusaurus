7cc2764a43c10644d04ab0207e9bbd83d762f71e
Neo Shi <shiwenxuan2003@gmail.com>
2021-08-31 12:13:30 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-    title: '软件工程',
+    title: '课程首页',
     tagline: '南开大学软件学院《软件工程》课程平台',
     url: 'https://se.nkugame.com',
     baseUrl: '/',
